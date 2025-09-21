e22c1b4d37890413d7a40e233624cc863abce7a7
avsimp <tim.ladaev@gmail.com>
2025-09-21 23:07:38 +0300

    all done all work plsplspls

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[]) {
 	char* word = "0";
 	char* file_name = "0";
-
+	
 
 	for (int i = 0; i < argc; i++){
 		if (strcmp(argv[i], "--word") == 0){
@@ -22,10 +22,36 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 
-	FILE *file = fopen(file_name, "r");
-	char cur_word[32] = {};
+	FILE* file = fopen(file_name, "r");
+	if (!file)
+    {
+        std::cout << "FAILED cant open file" << std::endl;
+		return 0;
+    }
+	const int WORD_LENGTH = 32;
+	char cur_word[WORD_LENGTH] = {};
+	int counter = 0;
 	while (fscanf(file, "%s", cur_word) == 1){
-		std::cout << cur_word;
+		bool contain_let = true;
+		for (int i = 0; word[i]!='\0';i++){
+			bool flag = false;
+			for (int j = 0; j < WORD_LENGTH && cur_word[j]!='\0'; j++){
+				//std::cout << word[i] << " " << cur_word[j] << std::endl;
+				if (word[i] == cur_word[j]){
+					flag = true;
+					break;
+				}
+			}
+			if (!flag){
+				contain_let = false;
+				break;
+			}
+		}
+		if (contain_let){
+			counter++;
+		}
 	}
+	fclose(file);
+	std::cout << counter << std::endl;
 	return 0;
 }
