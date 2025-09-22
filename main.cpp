3072ea356c7cef7c729f064118dd903a106ccf1e
avsimp <tim.ladaev@gmail.com>
2025-09-22 23:03:48 +0300

    review 1 fixes

--- main.cpp
+++ main.cpp
@@ -2,14 +2,42 @@
 #include <cstring>
 #include <fstream>
 
+const int kWorldLength = 32;
+
+bool WordHasAllLetters(char word[], char* base_word) {
+	for (int i = 0; base_word[i] != '\0';i++){
+		bool flag = false;
+		for (int j = 0; j < kWorldLength && word[j] != '\0'; j++){
+			if (base_word[i] == word[j]){
+				flag = true;
+				break;
+			}
+		}
+		if (!flag){
+			return false;
+		}
+	}
+	return true;
+}
+
+int CountWords(FILE* file, char* base_word) {
+	int counter = 0;
+	char cur_word[kWorldLength] = {};
+	while (fscanf(file, "%s", cur_word) == 1){
+		if (WordHasAllLetters(cur_word, base_word)){
+			counter++;
+		}
+	}
+	return counter;
+}
+
 int main(int argc, char* argv[]) {
-	char* word = "0";
-	char* file_name = "0";
-	
+	char* base_word = NULL;
+	char* file_name = NULL;
 
 	for (int i = 0; i < argc; i++){
 		if (strcmp(argv[i], "--word") == 0){
-			word = argv[i+1];
+			base_word = argv[i+1];
 			i++;
 		}
 		else if (strcmp(argv[i], "--file") == 0){
@@ -17,41 +45,20 @@ int main(int argc, char* argv[]) {
 			i++;
 		}
 	}
-	if (strcmp(word, "0") == 0 || strcmp(file_name, "0") == 0){
-		std::cout << "FAILED --word and --file arguments are required" << std::endl;
-		return 0;
+	//if (strcmp(word, '\0') == 0 || strcmp(file_name, '\0') == 0){
+	if (base_word == NULL || file_name == NULL){
+		std::cerr << "FAILED --word and --file arguments are required" << std::endl;
+		return 10;
 	}
 
 	FILE* file = fopen(file_name, "r");
-	if (!file)
-    {
-        std::cout << "FAILED cant open file" << std::endl;
-		return 0;
+	if (!file){
+        std::cerr << "FAILED cant open file" << std::endl;
+		return 20;
     }
-	const int WORD_LENGTH = 32;
-	char cur_word[WORD_LENGTH] = {};
-	int counter = 0;
-	while (fscanf(file, "%s", cur_word) == 1){
-		bool contain_let = true;
-		for (int i = 0; word[i]!='\0';i++){
-			bool flag = false;
-			for (int j = 0; j < WORD_LENGTH && cur_word[j]!='\0'; j++){
-				//std::cout << word[i] << " " << cur_word[j] << std::endl;
-				if (word[i] == cur_word[j]){
-					flag = true;
-					break;
-				}
-			}
-			if (!flag){
-				contain_let = false;
-				break;
-			}
-		}
-		if (contain_let){
-			counter++;
-		}
-	}
+
+	std::cout << CountWords(file, base_word) << std::endl;
 	fclose(file);
-	std::cout << counter << std::endl;
+
 	return 0;
 }
